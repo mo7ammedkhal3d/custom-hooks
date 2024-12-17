bce7a30102719da949d4e554226394c4cf95b951
cipher <mo7ammedkhaled@gmail.com>
2024-12-17 21:38:02 +0300

    some enhance

--- src/components/counters/Counters.js
+++ src/components/counters/Counters.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
-import ForwardCounter from "./ForwardCounter";
-import BackwardCounter from "./BackwardCounter";
-import classes from './Counters.modules.css';
+import ForwardCounter from './forwardCounter/ForwardCounter';
+import BackwardCounter from './backwardCounter/BackwardCounter';
+import classes from './Counters.module.css';
 
 const Counters = () =>{
     return (
