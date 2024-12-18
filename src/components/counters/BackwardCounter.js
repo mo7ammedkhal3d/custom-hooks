2fb7755129cb65229f33cab0ec88bbc208e6ad1b
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-12-18 22:20:42 +0300

    solved

---
+++ src/components/counters/BackwardCounter.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import Card from './Card';
+import useCounter from '../../hooks/use-counter';
+
+const BackwardCounter = () => {
+
+  const [rightAnimation,setRightAnimation] = useState(true);
+  const counter = useCounter(false)
+
+  useEffect(()=>{
+    const identifire = setTimeout(() => {
+      setRightAnimation(false);
+    }, 6000);
+
+  },[]);
+
+  return <Card className={rightAnimation ? 'right-anomation':''}>{counter}</Card>;
+};
+
+export default BackwardCounter;
